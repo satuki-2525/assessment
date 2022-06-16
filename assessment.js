43cc348043aef22eeb14608a7e51244a1f6e5f06
山吹早月 <satuki.book.2525@gmail.com>
2022-06-16 14:52:47 +0900

    Bootstrapでのデザイン改良

--- assessment.js
+++ assessment.js
@@ -21,14 +21,28 @@ const userName = userNameInput.value;
     
         // TODO 診断結果表示エリアの作成
         resultDivided.innerText = '';
-        const header = document.createElement('h3');  // ヘッダーにh3タグを動的に追加
-        header.innerText = '診断結果';                 // h3に挟んで表示
-        resultDivided.appendChild(header);
-    
-        const paragraph = document.createElement('p');  //動的にpタグを追加
-        const result = assessment(userName);
-        paragraph.innerText = result;
-        resultDivided.appendChild(paragraph);
+    // headerDivided の作成
+    const headerDivided = document.createElement('div');
+    headerDivided.setAttribute('class', 'card-header');
+    headerDivided.innerText = '診断結果';
+
+    // bodyDivided の作成
+    const bodyDivided = document.createElement('div');
+    bodyDivided.setAttribute('class', 'card-body');
+
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    bodyDivided.appendChild(paragraph);
+
+    // resultDivided に Bootstrap のスタイルを適用する
+    resultDivided.setAttribute('class', 'card');
+    resultDivided.setAttribute('style', 'max-width: 700px;')
+
+    // headerDivided と bodyDivided を resultDivided に差し込む
+    resultDivided.appendChild(headerDivided);
+    resultDivided.appendChild(bodyDivided);
     
         // TODO ツィートエリアの作成
         tweetDivided.innerText = '';
